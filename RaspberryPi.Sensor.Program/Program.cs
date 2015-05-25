6646b91ef97a87c24183eb39acb83a150649c2ed
Michael Glasgow <glasgowm@advisory.com>
2015-05-25 00:00:08 -0500

    Added a simple command line program for simple testing.

--- RaspberryPi.Sensor.Program/Program.cs
+++ RaspberryPi.Sensor.Program/Program.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RaspberryPi.Sensor.Program
+﻿namespace RaspberryPi.Sensor.Program
 {
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Threading;
+
     public class Program
     {
         public void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+
+            if (File.Exists("testFile.xml"))
+            {
+                Console.WriteLine("File exists");
+            }
+
+            if (Directory.Exists("TestDir"))
+            {
+                DirectoryInfo di = new DirectoryInfo("TestDir");
+                Debug.WriteLine(string.Format(" dir {0}", di.Name));
+            }
+
+            Thread.Sleep(300);
         }
     }
 }
